78a1f28cbc3b66ee4f177f9699fe22f3625a3e41
Eric <yg0803804@otc.edu>
2018-10-29 21:56:02 -0500

    add story

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+const chalk = require('chalk');
+
+console.log(chalk.yellow("  Pencil  ") + chalk.green("Author: Paulo Coelho"));
+console.log(chalk.blue("A boy was watching his grandmother write a letter. At one point he asked:"));
+
+console.log(chalk.red("‘Are you writing a story about what we’ve done? Is it a story about me?’"));
+
+console.log(chalk.blue("His grandmother stopped writing her letter and said to her grandson:"));
+
+console.log(chalk.cyan("I am writing about you, actually, but more important than the words is the pencil I’m using."));
+console.log(chalk.cyan("I hope you will be like this pencil when you grow up.’"));
+
+console.log(chalk.blue("Intrigued, the boy looked at the pencil. It didn’t seem very special.")); 
+console.log(chalk.red("‘But it’s just like any other pencil I’ve ever seen!’"));
+
+console.log(chalk.cyan("‘That depends on how you look at things. It has five qualities which," ));
+console.log(chalk.cyan("if you manage to hang on to them, will make you a person who is always at peace with the world.’"));
+
+console.log(chalk.magenta("‘First quality:") + "you are capable of great things, but you must never forget that there is"); 
+console.log("a hand guiding your steps. We call that hand God, and He always guides us according to His will.’");
+
+console.log(chalk.magenta("‘Second quality:") + " now and then, I have to stop writing and use a sharpener. That makes the"); 
+console.log("pencil suffer a little, but afterwards, he’s much sharper. So you, too, must learn to bear certain pains and sorrows, because they"); 
+console.log("will make you a better person.");
+
+console.log(chalk.magenta("‘Third quality:") + "the pencil always allows us to use an eraser to rub out any mistakes. This"); 
+console.log("means that correcting something we did is not necessarily a bad thing; it helps to keep us on the road to justice.’");
+
+console.log(chalk.magenta("‘Fourth quality:") + "what really matters in a pencil is not its wooden exterior, but the graphite");
+console.log("inside. So always pay attention to what is happening inside you.’");
+
+console.log(chalk.magenta("‘Finally,") + "the pencil’s fifth quality: it always leaves a mark. In just the same way, you"); 
+console.log("should know that everything you do in life will leave a mark, so try to be conscious of that in your every action.’");
